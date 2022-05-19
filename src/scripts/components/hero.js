cf5058060df1863a01b6790c9505e09e932dffff
Ibnu Choldun <96803344+ibnucholdun@users.noreply.github.com>
2022-05-19 11:41:48 +0700

    Update hero.js

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -7,7 +7,7 @@ class Hero extends HTMLElement {
     this.innerHTML = `
         <div class="hero">
         <div tabindex="0" class="hero_inner">
-            <h1 class="hero_title">Selamat Datang Di <span class="hero_bold">IT Resto</span></h1>
+            <h1 class="hero_title">Selamat Datang Di <span class="hero_bold">IC Resto</span></h1>
             <p class="hero_tagline">Kurus dan gendut adalah pilihan tapi makan enak adalah kebutuhan!</p>
             <a href="#main-content" class="hero_button">Baca Selengkapnya...</a>
         </div>
